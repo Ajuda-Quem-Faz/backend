ac7e57c2c0b1700f8a4325a5ec58543a7dcc2095
Matheus Teixeira <matheusdiegoteixeira@gmail.com>
2023-12-13 11:20:54 -0300

    create id

--- src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
+++ src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
@@ -1,23 +1,37 @@
 package com.generation.ajudaquemfaz.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table (name = "tb_categorias")
+@Table(name = "tb_categorias")
 public class Categoria {
-	
+
+	@Id // define as primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // define o Auto_Increment
+	private Long id;
+
 	@NotBlank(message = "O Atributo Setor é obrigatório")
 	@Size(max = 255, message = "O Atributo Setor deve conter no máximo 255 caractéres.")
 	private String setor;
 
 	@NotBlank(message = "O atributo tipo serviço é obrigatório")
-	@Size(max =255, message = "O Atributo tipo serviço deve conter no máximo 255 caracteres.")
+	@Size(max = 255, message = "O Atributo tipo serviço deve conter no máximo 255 caracteres.")
 	public String tipoServico;
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getSetor() {
 		return setor;
 	}
@@ -25,8 +39,7 @@ public class Categoria {
 	public void setSetor(String setor) {
 		this.setor = setor;
 	}
-	
-	
+
 	public String getTipoServico() {
 		return tipoServico;
 	}
@@ -34,5 +47,5 @@ public class Categoria {
 	public void setTipoServico(String tipoServico) {
 		this.tipoServico = tipoServico;
 	}
-	
+
 }
