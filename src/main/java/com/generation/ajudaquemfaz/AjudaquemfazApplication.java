153fc53fbec9a7b4d9418af549bbaf56ee1a3cd5
Wallysson Araujo <Cerberus74@live.com>
2023-12-13 09:43:20 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/ajudaquemfaz/AjudaquemfazApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ajudaquemfaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaquemfazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaquemfazApplication.class, args);
+	}
+
+}
