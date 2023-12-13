4f6e33d715eeef178ebfca939e6365dcb133ee68
Pablo Henrique <pablo.h.s.maia@gmail.com>
2023-12-13 10:43:52 -0300

    Atributo Setor da Model Categoria

--- src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
+++ src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
@@ -2,9 +2,25 @@ package com.generation.ajudaquemfaz.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table (name = "tb_categorias")
 public class Categoria {
+	
+	@NotBlank(message = "O Atributo Setor é obrigatório")
+	@Size(max = 255, message = "O Atributo Setor deve conter no máximo 255 caractéres")
+	private String setor;
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
+	
 
 }
