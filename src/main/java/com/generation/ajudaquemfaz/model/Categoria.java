ace70528866925684f9813288cba97b610a7f1c3
AlineConchetta <alineconchetta@gmail.com>
2023-12-13 10:27:47 -0300

    Classe Model Categoria

---
+++ src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
@@ -0,0 +1,10 @@
+package com.generation.ajudaquemfaz.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tb_categorias")
+public class Categoria {
+
+}
