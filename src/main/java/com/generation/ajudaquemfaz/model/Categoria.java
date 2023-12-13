d82ab47cf8569d5b983564773ebdf5fd6db8079f
Vitor Ferreira <vitornasc673@gmail.com>
2023-12-13 10:42:57 -0300

    Implantação do atributo tipoServiço

--- src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
+++ src/main/java/com/generation/ajudaquemfaz/model/Categoria.java
@@ -2,9 +2,23 @@ package com.generation.ajudaquemfaz.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table (name = "tb_categorias")
 public class Categoria {
 
+	@Size(max =255, message = "O tipo serviço deve conter entre 5 e 255 caracteres.")
+	@NotBlank
+	public String tipoServico;
+
+	public String getTipoServico() {
+		return tipoServico;
+	}
+
+	public void setTipoServico(String tipoServico) {
+		this.tipoServico = tipoServico;
+	}
+	
 }
