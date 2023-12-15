d133218eb1b75f706e91b488a03f44050ba18b44
Pablo Henrique <pablo.h.s.maia@gmail.com>
2023-12-15 11:32:29 -0300

    Criação do Método Delete

--- src/main/java/com/generation/ajudaquemfaz/controller/CategoriaController.java
+++ src/main/java/com/generation/ajudaquemfaz/controller/CategoriaController.java
@@ -1,17 +1,20 @@
 package com.generation.ajudaquemfaz.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -27,14 +30,14 @@ public class CategoriaController {
 
 	@Autowired
 	public CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping("/setor/{setor}")
-	public ResponseEntity<List<Categoria>> getAllBySetor(@PathVariable String setor){
+	public ResponseEntity<List<Categoria>> getAllBySetor(@PathVariable String setor) {
 		return ResponseEntity.ok(categoriaRepository.findAllBySetorContainingIgnoreCase(setor));
 	}
-	
+
 	@GetMapping("/servico/{servico}")
-	public ResponseEntity<List<Categoria>> getAllByServico(@PathVariable String servico){
+	public ResponseEntity<List<Categoria>> getAllByServico(@PathVariable String servico) {
 		return ResponseEntity.ok(categoriaRepository.findAllByTipoServicoContainingIgnoreCase(servico));
 	}
 
@@ -42,28 +45,36 @@ public class CategoriaController {
 	public ResponseEntity<List<Categoria>> getAll() {
 		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
-	
-	//Método para Criação da categoria
+
+	// Método para Criação da categoria
 	@PostMapping
 	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
-			return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
 	}
-	
-	//Método para Atualização da categoria
+
+	// Método para Atualização da categoria
 	@PutMapping
-	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria){
-		if(categoriaRepository.existsById(categoria.getId()))
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+		if (categoriaRepository.existsById(categoria.getId()))
 			return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.save(categoria));
-		
+
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não existe.", null);
 	}
-	
-	//Método para pegar Categoria por Id
+
+	// Método para pegar Categoria por Id
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> getById(@PathVariable Long id) {
-		return categoriaRepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta))
+		return categoriaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-	
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		if (categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		categoriaRepository.deleteById(id);
+	}
+
 }
