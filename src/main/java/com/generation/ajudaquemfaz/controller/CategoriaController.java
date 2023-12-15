5b7daba0ac38fa15a8ab47e3669eddb3cd907c50
Vitor Ferreira <vitornasc673@gmail.com>
2023-12-15 11:59:23 -0300

    Inserção de comentários

--- src/main/java/com/generation/ajudaquemfaz/controller/CategoriaController.java
+++ src/main/java/com/generation/ajudaquemfaz/controller/CategoriaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.generation.ajudaquemfaz.model.Categoria;
 import com.generation.ajudaquemfaz.repository.CategoriaRepository;
 
@@ -28,31 +27,35 @@ import jakarta.validation.Valid;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
+	// Método para injeção de dependências
 	@Autowired
 	public CategoriaRepository categoriaRepository;
 
+	// Método para buscar Categoria por setor
 	@GetMapping("/setor/{setor}")
 	public ResponseEntity<List<Categoria>> getAllBySetor(@PathVariable String setor) {
 		return ResponseEntity.ok(categoriaRepository.findAllBySetorContainingIgnoreCase(setor));
 	}
 
+	// Método para buscar Categoria por serviço
 	@GetMapping("/servico/{servico}")
 	public ResponseEntity<List<Categoria>> getAllByServico(@PathVariable String servico) {
 		return ResponseEntity.ok(categoriaRepository.findAllByTipoServicoContainingIgnoreCase(servico));
 	}
 
+	// Método para listar todos as Categorias
 	@GetMapping
 	public ResponseEntity<List<Categoria>> getAll() {
 		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
 
-	// Método para Criação da categoria
+	// Método para Criação da Categoria
 	@PostMapping
 	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
 	}
 
-	// Método para Atualização da categoria
+	// Método para Atualização da Categoria
 	@PutMapping
 	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
 		if (categoriaRepository.existsById(categoria.getId()))
@@ -61,13 +64,14 @@ public class CategoriaController {
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não existe.", null);
 	}
 
-	// Método para pegar Categoria por Id
+	// Método para buscar Categoria por Id
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> getById(@PathVariable Long id) {
 		return categoriaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 
+	// Método para deletar Categoria por Id
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
