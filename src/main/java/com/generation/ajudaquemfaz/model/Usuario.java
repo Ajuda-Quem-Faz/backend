0fee20256e2a4198a5b22408f8f3f8ed8b63be20
Wallysson Araujo <Cerberus74@live.com>
2023-12-18 10:53:47 -0300

    Class Model Usuario

---
+++ src/main/java/com/generation/ajudaquemfaz/model/Usuario.java
@@ -0,0 +1,72 @@
+package com.generation.ajudaquemfaz.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	private String nome;
+	
+	@Email
+	@NotBlank(message = "O atributo usuario é obrigatório!")
+	private String usuario;
+	
+	private String foto;
+	
+	@NotBlank(message = "O atributo senha é obrigatório!")
+	@Size(min = 8, message = "O Atributo senha deve conter no mínimo 8 caracteres!")
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
