291307faf6ef4ebd6506f926d190d6f86017c95a
unknown <luiza.kormann@gmail.com>
2023-12-18 11:38:20 -0300

    definição de atritutos da classe model de Produto

--- src/main/java/com/generation/ajudaquemfaz/model/Produto.java
+++ src/main/java/com/generation/ajudaquemfaz/model/Produto.java
@@ -1,12 +1,77 @@
 package com.generation.ajudaquemfaz.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="tb_produtos")
+@Table(name = "tb_produtos")
 public class Produto {
-	
-	// TODO : criar atributos da model 'Produtos'
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O campo nome é obrigatório")
+	private String nome;
+
+	@Column(length = 500)
+	@Size(max = 500, message = "A descrição não pode ultrapassar dos 500 caracteres.")
+	private String descricao;
+
+	@Digits(integer = 7, fraction = 2)
+	@DecimalMin(value = "0.5", message = "O preço mínimo dos produtos é R$ 0,50")
+	private BigDecimal preco;
+
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
 }
