38d7337c2c14ac66dcd98971bb92058c4a2dd488
unknown <luiza.kormann@gmail.com>
2023-12-18 13:50:37 -0300

    métodos CRUD recurso Produto: Post

--- src/main/java/com/generation/ajudaquemfaz/controller/ProdutoController.java
+++ src/main/java/com/generation/ajudaquemfaz/controller/ProdutoController.java
@@ -1,14 +1,36 @@
 package com.generation.ajudaquemfaz.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.ajudaquemfaz.model.Produto;
+import com.generation.ajudaquemfaz.repository.CategoriaRepository;
+import com.generation.ajudaquemfaz.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
-	// TODO : criar CRUD e métodos específicos da classe 'Produtos'
-	
-}
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+		if (categoriaRepository.existsById(produto.getCategoria().getId()))
+			return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto));
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe", null);
+	}
+} 
