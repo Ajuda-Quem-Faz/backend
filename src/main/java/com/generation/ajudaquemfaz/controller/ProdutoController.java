179dd5c4595d206d47dcab28b383d5e91dd1b35e
AlineConchetta <alineconchetta@gmail.com>
2023-12-18 13:41:17 -0300

    Método DeleteMapping e GetMappingNome

--- src/main/java/com/generation/ajudaquemfaz/controller/ProdutoController.java
+++ src/main/java/com/generation/ajudaquemfaz/controller/ProdutoController.java
@@ -1,15 +1,19 @@
 package com.generation.ajudaquemfaz.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.ajudaquemfaz.model.Produto;
 import com.generation.ajudaquemfaz.repository.ProdutoRepository;
@@ -30,10 +34,25 @@ public class ProdutoController {
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
+	@GetMapping("/nome/{nome}")
+	ResponseEntity<List<Produto>> getByProduto(@PathVariable String nome) {
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
 	// Método para listar todos as Categorias
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll() {
 		return ResponseEntity.ok(produtoRepository.findAll());
 	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete (@PathVariable Long id) {
+		Optional<Produto> testa = produtoRepository.findById(id);
+		if (testa.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		produtoRepository.deleteById(id);
+		}
 	
 }
